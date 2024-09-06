470974d33a26c0c05569e26ddd35ab6f9a6f613f
Ko-Arimura <gyoucun000@gmail.com>
2024-09-06 12:08:06 +0900

    js2

--- js/data.js
+++ js/data.js
@@ -50,3 +50,35 @@ switch (num3) {
     console.log('big hit')
     break;
 }
+let num4 = Math.floor(Math.random()*5);
+console.log('first number is'+ num4);
+
+while (num4 !== 0) {
+  num4 = Math.floor(Math.random()*5);
+  console.log('now number is'+num4);
+}
+
+for (let i =1; i <= 5; i++) {
+  console.log(i);
+}
+const userNames =['侍太郎', '侍一郎', '侍二郎', '侍三郎', '侍四郎'];
+console.log(userNames);
+userNames[1] = 'none';
+userNames[5] ='jon';
+console.log(userNames);
+
+const personalData ={ age:36,gender:"men"};
+console.log(personalData);
+personalData.age =37;
+personalData.address = "tokyo";
+console.log(personalData);
+
+const sayGoodMorning =() => {
+  console.log("hello");
+  console.log("good morning");
+}
+const sayGoodEvening = () => {
+  console.log("good evening");
+  console.log("good night");
+}
+sayGoodEvening();
